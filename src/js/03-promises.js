e06dc9749592ebcc13144cab15bf893deee97a5f
Serhei Honcharov <mrsedmor@gmail.com>
2022-10-08 23:59:32 +0200

    task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,47 @@
+import Notiflix from 'notiflix';
+
+const formElem = document.querySelector('.form');
+
+formElem.addEventListener('submit', onFormSubmit);
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      }
+
+      reject({ position, delay });
+    }, delay);
+  });
+}
+
+function createPromises({ delay, step, amount }) {
+  for (let i = 1, iDelay = delay; i <= amount; i += 1, iDelay += step) {
+    createPromise(i, iDelay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
   }
+}
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const formData = new FormData(event.currentTarget);
+  const data = {
+    delay: Number(formData.get('delay')),
+    step: Number(formData.get('step')),
+    amount: Number(formData.get('amount')),
+  };
+
+  createPromises(data);
 }
